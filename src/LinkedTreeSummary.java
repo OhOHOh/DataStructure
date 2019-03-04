bd2a87382ccb67e5e666a1ab20c5f24739e3ae10
Planck <15221589025@163.com>
2019-03-04 23:26:42 +0800

    二叉树

--- src/LinkedTreeSummary.java
+++ src/LinkedTreeSummary.java
@@ -1,13 +1,13 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
- *  关于java中二叉树的操作
+ * 关于java中二叉树的操作
  * 1. 二叉树的几种遍历
  * 2. 求二叉搜索树中的第K大的结（findKthNode）
  * 3. 根据前序遍历+中序遍历来构建二叉树
  * 4. 如何构建一颗二叉查找树(二叉搜索树)(二叉排序树)、判断数组是否是一颗二叉查找树的后续遍历
+ * 5. 如何实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
  */
 
 public class LinkedTreeSummary {
@@ -20,6 +20,7 @@ public class LinkedTreeSummary {
             this.val = val;
         }
     }
+
     int KthNode_index = 0;
 
     //========================Java 二叉树的几种遍历========================
@@ -41,7 +42,7 @@ public class LinkedTreeSummary {
          */
         Stack<TreeNode> s = new Stack<>();
         while (true) {
-            while(root != null) {  // 入栈
+            while (root != null) {  // 入栈
                 System.out.println(root.val);
                 s.push(root);
                 root = root.left;
@@ -103,7 +104,7 @@ public class LinkedTreeSummary {
         s.push(root);
         while (!s.isEmpty()) {
             root = s.peek();
-            if (root.left==null && root.right==null) {
+            if (root.left == null && root.right == null) {
                 System.out.println(root.val);
                 s.pop();
                 continue;
@@ -136,24 +137,25 @@ public class LinkedTreeSummary {
 
 
     public TreeNode findKthNode(TreeNode root, int k) {
-        if(root != null){ //中序遍历寻找第k个
-            TreeNode node = findKthNode(root.left,k);
-            if(node != null)
+        if (root != null) { //中序遍历寻找第k个
+            TreeNode node = findKthNode(root.left, k);
+            if (node != null)
                 return node;
-            KthNode_index ++;
-            if(KthNode_index == k)
+            KthNode_index++;
+            if (KthNode_index == k)
                 return root;
-            node = findKthNode(root.right,k);
-            if(node != null)
+            node = findKthNode(root.right, k);
+            if (node != null)
                 return node;
         }
         return null;
     }
+
     public TreeNode reConstructTree(int[] pre, int[] in) {
         /**
          * 给出一颗二叉树的前序遍历与中序遍历, 重建这棵树
          */
-        TreeNode root = reConstructTree_fun(pre, 0, pre.length-1, in, 0, in.length-1);
+        TreeNode root = reConstructTree_fun(pre, 0, pre.length - 1, in, 0, in.length - 1);
         return root;
     }
     private TreeNode reConstructTree_fun(int[] pre, int preStart, int preEnd, int[] in, int inStart, int inEnd) {
@@ -163,8 +165,8 @@ public class LinkedTreeSummary {
         TreeNode root = new TreeNode(pre[preStart]);
         for (int i = inStart; i <= inEnd; i++) {
             if (pre[preStart] == in[i]) {
-                root.left = reConstructTree_fun(pre, preStart+1, i-inStart+preStart, in, inStart, i-1);
-                root.right = reConstructTree_fun(pre, preStart+1+i-inStart, preEnd, in, i+1, inEnd);
+                root.left = reConstructTree_fun(pre, preStart + 1, i - inStart + preStart, in, inStart, i - 1);
+                root.right = reConstructTree_fun(pre, preStart + 1 + i - inStart, preEnd, in, i + 1, inEnd);
             }
         }// for
         return root;
@@ -181,7 +183,7 @@ public class LinkedTreeSummary {
             TreeNode head = root;
             TreeNode a = new TreeNode(arr[i]);
             while (head != null) {
-                if (head.left==null || head.right==null) {
+                if (head.left == null || head.right == null) {
                     tmp = head;
                 }
                 if (arr[i] < head.val) {
@@ -198,18 +200,19 @@ public class LinkedTreeSummary {
         }//for
         return root;
     }
+
     public boolean verifyPostOrderBST(int[] a) {
         /**
          * 判断数组是否是一颗二叉查找树的后续遍历
          */
-        if (a==null || a.length<=0) {
+        if (a == null || a.length <= 0) {
             return false;
         }
         if (a.length == 1) {
             return true;
         }
 
-        return verifyPostOrderBST_fun(a, 0, a.length-1);
+        return verifyPostOrderBST_fun(a, 0, a.length - 1);
     }
     private boolean verifyPostOrderBST_fun(int[] a, int low, int high) {
         if (low >= high) {
@@ -226,10 +229,67 @@ public class LinkedTreeSummary {
             }
             j++;
         }
-        boolean left = verifyPostOrderBST_fun(a, low, i-1);
-        boolean right = verifyPostOrderBST_fun(a, i, high-1);
+        boolean left = verifyPostOrderBST_fun(a, low, i - 1);
+        boolean right = verifyPostOrderBST_fun(a, i, high - 1);
 
-        return left&&right;
+        return left && right;
     }
 
+    private ArrayList<ArrayList<Integer>> Print_Z(TreeNode root) {
+        /**
+         * 实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印
+         * 其他行以此类推。
+         * 用双端队列来做, 每次近队列的方向是不变的, 但是出队列的方向每次都变换
+         */
+//        int i = 0;
+//        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+//        Deque<TreeNode> deque = new LinkedList<>();
+//        deque.addFirst(root);
+//        TreeNode last = root;
+//        while (!deque.isEmpty()) {// 每次循环一层
+//
+//        }
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        s1.push(root);
+        while (!s1.isEmpty() || !s2.isEmpty()) {// 每一层一个大循环
+            ArrayList<Integer> tmp = new ArrayList<>();
+            if (!s1.isEmpty()) {
+                while (!s1.isEmpty()) {
+                    TreeNode tmpNode = s1.pop();
+                    tmp.add(tmpNode.val);
+                    if (tmpNode.left != null) s2.push(tmpNode.left);
+                    if (tmpNode.right != null) s2.push(tmpNode.right);
+
+                }//while
+                result.add(tmp);
+            } else {
+                while (!s2.isEmpty()) {
+                    TreeNode tmpNode = s2.pop();
+                    tmp.add(tmpNode.val);
+                    if (tmpNode.right != null) s1.push(tmpNode.right);
+                    if (tmpNode.left != null) s1.push(tmpNode.left);
+                }// while
+                result.add(tmp);
+            }
+        }
+        return result;
+    }
+    ArrayList<ArrayList<Integer> > Print(TreeNode root) {
+        /**
+         * 打印二叉树, 每层都是一个数组, 而不是一棵树对应一个数组
+         */
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+
+        }
+
+        return result;
+    }
 }
