2acd38ed7bd950e654c43c2ea2fbaa0b9cb2928d
Planck <15221589025@163.com>
2019-03-10 18:37:30 +0800

    IO操作

--- src/in_test.java
+++ src/in_test.java
@@ -6,17 +6,26 @@ public class in_test {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
-        List<Integer> a = new ArrayList<>();
-//        while (in.hasNext()) {
-//            a.add(in.next());
-//        }
-//
-//        for (int i = 0; i < a.size(); i++) {
-//            System.out.println(a.get(i));
-//        }
-        String b = in.nextLine();
-        System.out.println(b);
+        // 输入不定长的数组, 使用 数组来实现
+        List<Integer> list = new ArrayList<Integer>();
+        String s = in.nextLine();
+        String[] str = s.split(" ");
+        int[] a = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            a[i] = Integer.parseInt(str[i]);
+        }
+        for (int e: a) {
+            System.out.print(e + " ");
+        }
 
-
+//        // 输入定长的数组, 先输入数组长度 n
+//        int n = in.nextInt();
+//        int[] array1 = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            array1[i] = in.nextInt();
+//        }
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(array1[i] + " ");
+//        }
     }
 }
